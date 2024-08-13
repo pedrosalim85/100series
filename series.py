b1e873032f6f8e131c9f7d1d194086dd8737e7c6
Pedro Salim Miranda <157762887+pedrosalim85@users.noreply.github.com>
2024-08-13 09:40:33 -0300

    Add files via upload

---
+++ series.py
@@ -0,0 +1,24 @@
+import pandas as pd
+import numpy as np
+
+series = pd.read_csv('C:/Users/Pedro/Desktop/PROGRAMACAO/DADOS/hundredseriebox.csv')
+
+df_series = pd.DataFrame(series)
+
+#print(df_series.sort_values('score', ascending=False).head(20))
+
+terror = df_series[(df_series['genre'] == 'Terror')]
+#print(terror)
+
+df_series['is_american'] = df_series['place'].str.contains('USA')
+
+terror_filter = df_series['genre'] == 'Terror'
+american_filter = df_series['is_american']
+
+terror_american = df_series[terror_filter & american_filter] #series de terror americanas
+
+#print(terror_american)
+
+generos = series.groupby('genre')
+print(generos['score'].aggregate(np.mean))   #media de score por genero
+print(generos['score'].aggregate(np.sum).max())
